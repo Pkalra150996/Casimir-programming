2ee6a25f1d3a94f47cf808c355cdcf40ae6b2525
Priyanshi Kalra <priyanshikalra15@gmail.com>
2022-01-31 16:30:53 +0100

    world

--- text.py
+++ text.py
@@ -1,4 +1,13 @@
 
 print('hello world')
-
+# calculate the circumference of circle.
+PI = 3.14
+r = float(input("Enter the radius of a circle:"))
+area = PI * r * r
+print("Area of a circle = %.2f" %area)
+#calulate the surface area of a circle.
+def findArea(r):
+    PI = 3.14
+    return PI * (r*r);
+print("Area of circle = %.6f" % findArea(6));
 
